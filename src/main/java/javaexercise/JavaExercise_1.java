8b6d89365a5e642dd7b890ca18cfcb822c88dead
heejunns <wns2252@hanmail.net>
2022-11-13 03:29:45 +0900

    while문 실습

---
+++ src/main/java/javaexercise/JavaExercise_1.java
@@ -0,0 +1,11 @@
+package javaexercise;
+
+public class JavaExercise_1 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while(i-- != 0){
+            System.out.println(i + " -  I can do it.");
+        }
+    }
+}
