30f1dbaf409e0835c64ee17620d0c5efbfb32694
heejunns <wns2252@hanmail.net>
2022-11-17 22:14:10 +0900

    이름붙은 반복문 실습

--- src/main/java/javaexercise/JavaExercise_8.java
+++ src/main/java/javaexercise/JavaExercise_8.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class JavaExercise_8 {
     public static void main(String[] args) { // 이름붙은 반복문 실습
 
-        loop : for (int i=1; i<=5; i++){
+        loop: for (int i=1; i<=5; i++){
+
             for (int j=1; j<=5; j++){
-                if(j==6)
+                if(j==3) {
                     break loop;
                     //break;
+                }
                 System.out.println(i+"*"+j+"="+ i*j);
             }
             System.out.println();
