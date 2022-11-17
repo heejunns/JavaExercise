a3082a5f5939c420fa837e4a0ae2976d36a9a9c7
heejunns <wns2252@hanmail.net>
2022-11-17 21:52:20 +0900

    0 부터 값을 1씩 증가하여 모두 더하였을때 300이 넘지 않는 최대 값과 증가한 값을 알아내는 프로그램

--- src/main/java/javaexercise/JavaExercise_2.java
+++ src/main/java/javaexercise/JavaExercise_2.java
@@ -1,6 +1,6 @@
 package javaexercise;
 // 1부터 더하기를 계속 진행 하였을 때 300을 넘지 않는 최대 값과 몇까지 더하였는지 알아내는 프로그램
-public class JavaExercise_2 {
+public class JavaExercise_2 { // 0 부터 값을 1씩 증가하여 모두 더하였을때 300이 넘지 않는 최대 값과 증가한 값을 알아내는 프로그램
     public static void main(String[] args) {
         int i = 0;
         int sumTotal = 0;
@@ -9,7 +9,7 @@ public class JavaExercise_2 {
             sumTotal+= ++i;
         }
         System.out.println();
-        System.out.printf("1부터 값을 모두 더하였을때 300을 넘지 않는 최대 값과 %d이고 %d까지 더하였습니다.",sumTotal-i,i-1);
+        System.out.printf("1부터 값을 모두 더하였을때 300을 넘지 않는 최대 값은 %d이고 %d까지 더하였습니다.",sumTotal-i,i-1);
 
     }
 }
