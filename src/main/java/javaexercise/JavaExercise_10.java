b23c84849089df786b6ad90474469d79259ce397
heejunns <wns2252@hanmail.net>
2022-11-18 19:59:58 +0900

    배열 실습2 (배열의 길이 length 사용)

--- src/main/java/javaexercise/JavaExercise_10.java
+++ src/main/java/javaexercise/JavaExercise_10.java
@@ -6,8 +6,8 @@ public class JavaExercise_10 {
 
         System.out.println("배열의 길이는 : "+i.length+" 입니다.");
 
-        for(int l = 0; l < i.length; l++){ // i.length를 사용하면 혹시나 배열의 길이를 변경하고 for문의 조건식을 변경하지 못하였어도 arrayaoutofboundsexception 에러가 발생할 일이 없다.
-            System.out.printf("i[%d] = %d\n",l,i[l]);
+        for(int l = 0; l < i.length; l++) { // i.length를 사용하면 혹시나 배열의 길이를 변경하고 for문의 조건식을 변경하지 못하였어도 arrayaoutofboundsexception 에러가 발생할 일이 없다.
+            System.out.printf("i[%d] = %d\n", l, i[l]);
         }
 
     }
