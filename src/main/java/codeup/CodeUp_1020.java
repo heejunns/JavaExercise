f33d598a7ebd4c9dcc2f6dd67cc2f143c6a9104a
heejunns <wns2252@hanmail.net>
2022-11-22 22:07:11 +0900

    codeup 1020번

---
+++ src/main/java/codeup/CodeUp_1020.java
@@ -0,0 +1,11 @@
+package codeup;
+import java.util.Scanner;
+
+public class CodeUp_1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.printf("%s%s",str.substring(0,6),str.substring(7));
+
+    }
+}
