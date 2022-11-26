fa0bd04cbd572af73cc6029517b79660c9b6c66e
heejunns <wns2252@hanmail.net>
2022-11-27 04:38:10 +0900

    Object 클래스의 equals() 오버라이딩

---
+++ src/main/java/javaexercise/JavaExercise_54.java
@@ -0,0 +1,27 @@
+package javaexercise;
+class Value {
+    int value;
+
+    Value(int value ){
+        this.value = value;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Value) { // obj가 Value로 형 변환이 가능한지 판단
+            Value v = (Value) obj; // 참이라면 obj의 타입은 Object이기 때문에 Value로 형변환
+            return this.value == v.value; // 값이 같다면 true, 다르다면 false
+        } else {
+            return false; // 형변환이 불가능하다면 false
+        }
+    }
+}
+class JavaExercise_54 {
+    public static void main(String[] args) { // Object 클래스의 equals() 오버라이딩
+        Value v1 = new Value(20);
+        Value v2 = new Value(20);
+
+        System.out.println(v1.equals(v2));
+
+    }
+}
+
