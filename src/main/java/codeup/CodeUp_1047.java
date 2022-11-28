e9afe7ab8e4d561208e1c76ab06fc2f2c1745c48
heejunns <wns2252@hanmail.net>
2022-11-28 19:03:58 +0900

    codeup 1047번

---
+++ src/main/java/codeup/CodeUp_1047.java
@@ -0,0 +1,10 @@
+package codeup;
+import java.util.Scanner;
+
+public class CodeUp_1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(n*2);
+    }
+}
