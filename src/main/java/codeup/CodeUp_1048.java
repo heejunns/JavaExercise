f3a39e6e3870ba525c718768c184b77427e960bc
heejunns <wns2252@hanmail.net>
2022-11-29 22:08:05 +0900

    codeup 1048번

---
+++ src/main/java/codeup/CodeUp_1048.java
@@ -0,0 +1,12 @@
+package codeup;
+import java.util.Scanner;
+
+public class CodeUp_1048 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int n2  = sc.nextInt();
+            System.out.println((int)(n*(Math.pow(2,n2))));
+        }
+    }
+
