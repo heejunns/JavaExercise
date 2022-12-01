fa186121660c9dbea448ae4a2ce7966ef6f126ad
heejunns <wns2252@hanmail.net>
2022-12-01 19:39:24 +0900

    codeup 1063번

---
+++ src/main/java/codeup/CodeUp_1063.java
@@ -0,0 +1,12 @@
+package codeup;
+import java.util.Scanner;
+
+public class CodeUp_1063 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            System.out.println(Math.max(n1,n2));
+        }
+}
+
