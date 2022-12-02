a98889b23fffa0a3f07ea5ede5043a74a4403627
heejunns <wns2252@hanmail.net>
2022-12-02 22:07:31 +0900

    codeup 1066번

---
+++ src/main/java/codeup/CodeUp_1066.java
@@ -0,0 +1,20 @@
+package codeup;
+import java.util.Scanner;
+
+public class CodeUp_1066 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int n3 = sc.nextInt();
+            int[] arr = {n1, n2, n3};
+            for (int i = 0; i< arr.length; ++i){
+                if (arr[i]%2 == 0 ){
+                    System.out.println("even");
+                }else{
+                    System.out.println("odd");
+                }
+            }
+        }
+    }
+
