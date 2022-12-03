3261e3788933042fd374e91f0e42437f4f799ff4
heejunns <wns2252@hanmail.net>
2022-12-03 22:12:00 +0900

    HashSet 클래스 (HashSet 생성해보고 객체 저장 해보기 후에 iterator()로 다시 읽어오기)

--- src/main/java/javaexercise/JavaExercise_69.java
+++ src/main/java/javaexercise/JavaExercise_69.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 class JavaExercise_69 {
 
-    public static void main(String[] args) { // HashSet 클래스
+    public static void main(String[] args) { // HashSet 클래스 (HashSet 생성해보고 객체 저장 해보기 후에 iterator()로 다시 읽어오기)
      Object[] objectArr = {"10", new Integer(6),"1","2","2","2","5","6","3","5","20"};
      Set set = new HashSet();
 
