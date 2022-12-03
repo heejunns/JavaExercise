ae63ee0c2f2657609410f78f931ff1699ab92660
heejunns <wns2252@hanmail.net>
2022-12-03 13:10:07 +0900

    codeup 1071번

---
+++ src/main/java/codeup/CodeUp_1071.java
@@ -0,0 +1,23 @@
+package codeup;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CodeUp_1071 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        while(true){
+            int n = sc.nextInt();
+            if (n==0){
+                break;
+            }
+            list.add(n);
+        }
+        for (int i : list){
+            System.out.println(i);
+        }
+
+
+    }
+}
+
