f6d26e2695ae802c80c976597dc9e44e6ca0dc29
heejunns <wns2252@hanmail.net>
2022-12-03 22:40:30 +0900

    HashSet 클래스 2 ( 랜덤한 45개의 중복되지 않은 6개의 수를 HashSet에 저장하기, 저장과정 for,while문 두개 다 구현해보기)

--- src/main/java/javaexercise/JavaExercise_70.java
+++ src/main/java/javaexercise/JavaExercise_70.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 
 class JavaExercise_70 {
-    public static void main(String[] args) { // HashSet 클래스 2 ( 랜던한 45개의 중복되지 않은 6개의 수를 HashSet에 저장하기, 저장과정 for,while문 두개 다 구현해보기)
+    public static void main(String[] args) { // HashSet 클래스 2 ( 랜덤한 45개의 중복되지 않은 6개의 수를 HashSet에 저장하기, 저장과정 for,while문 두개 다 구현해보기)
         Set set = new HashSet();
 //        for(int i=0; set.size() <6; i++){
 //            int n = (int)(Math.random()*45)+1;
