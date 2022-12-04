dbd6b9041ca77b154a1e703ff993b0dcff9a6d51
heejunns <wns2252@hanmail.net>
2022-12-04 09:10:06 +0900

    codeup 1074번

---
+++ src/main/java/codeup/CodeUp_1074.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUp_1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n; i>0; --i){
+            System.out.println(i);
+        }
+
+    }
+}
+
