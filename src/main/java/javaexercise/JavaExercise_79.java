d193e13f3ff5971fd66a4b2d476993cc56af5404
heejunns <wns2252@hanmail.net>
2022-12-08 15:36:27 +0900

    지네릭스 사용해보기 (지네릭스 다형성)

--- src/main/java/javaexercise/JavaExercise_79.java
+++ src/main/java/javaexercise/JavaExercise_79.java
@@ -25,9 +25,9 @@ class JavaExercise_79 {
         Soccer s2 = (Soccer) list.get(1); // 반환타입이 Sport 이기 때문에 Soccer 타입 변수에 저장할려면 형변환을 해줘야함
         Baseball b = (Baseball) list.get(2); // 반환타입이 Sport 이기 때문에 Baseball 타입 변수에 저장할려면 형변환을 해줘야함
 
-       JavaExercise_79 j = new JavaExercise_79();
-       j.printAll(list);
-    //   j.printAll(listSoccer); 컴파일 에러가 발생한다. printAll() 메서드의 입력 매개변수의 타입이 Sport 이기 때문이다.
+        JavaExercise_79 j = new JavaExercise_79();
+        j.printAll(list);
+        //   j.printAll(listSoccer); 컴파일 에러가 발생한다. printAll() 메서드의 입력 매개변수의 타입이 Sport 이기 때문이다.
     }
     public void printAll(List<Sport> l){
         for(Sport s : l){
