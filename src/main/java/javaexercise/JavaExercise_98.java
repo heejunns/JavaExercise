1c42d20751456251a5076ec9e6b5c67a65f3f7c9
heejunns <wns2252@hanmail.net>
2022-12-14 16:02:53 +0900

    interrupt() 메서드 사용해보기, 예외처리해서 종료되지 않게 한 후 다시 인터럽트 발생해 쓰레드 종료 시키기

---
+++ src/main/java/javaexercise/JavaExercise_98.java
@@ -0,0 +1,43 @@
+package javaexercise;
+
+class Exercise13Thread extends Thread {
+    public void run(){
+        for(int i=0; i<300; ++i){
+            System.out.print(new String("-"));
+        }
+    }
+}
+class Exercise14Thread extends Thread {
+    public void run() {
+        for (int i = 0; i < 300; ++i) {
+            System.out.print(new String("|"));
+        }
+
+    }
+}
+
+class JavaExercise_98 {
+    static long startTime = 0;
+
+    public static void main(String[] args) { // join()
+        Exercise13Thread exercise13Thread = new Exercise13Thread();
+        Exercise14Thread exercise14Thread = new Exercise14Thread();
+
+        exercise13Thread.start();
+        exercise14Thread.start();
+
+        try {
+            exercise13Thread.join();
+            exercise14Thread.join();
+        } catch (InterruptedException e){
+
+        }
+        System.out.println("main 쓰레드가 종료되었습니다.");
+
+
+
+    }
+
+}
+
+
